46820ed2ecf683347199766c8cf619883be0801f
Philip R. Kensche <kensche@dkfz-heidelberg.de>
2016-02-22 16:37:51 +0100

    Fixed return values of ACEseqMethods.

--- src/de/dkfz/b080/co/qcworkflow/ACEseqMethods.groovy
+++ src/de/dkfz/b080/co/qcworkflow/ACEseqMethods.groovy
@@ -4,29 +4,38 @@ import de.dkfz.b080.co.files.*;
 import de.dkfz.roddy.execution.jobs.*;
 import de.dkfz.roddy.knowledge.files.Tuple2;
 import de.dkfz.roddy.knowledge.files.Tuple3;
-import de.dkfz.roddy.knowledge.methods.GenericMethod;
+import de.dkfz.roddy.knowledge.methods.GenericMethod
 
 /**
  * Created by kleinhei on 6/16/14.
  */
 @StaticScriptProviderClass
-public final class AceSeqQC {
+public final class ACEseqMethods {
 
 
     @ScriptCallingMethod
-    public static TextFile annotateCovWindows(CoverageTextFile coverage1kbFile) {
-        return (TextFile) GenericMethod.callGenericTool(COConstants.TOOL_ADD_HAPLOTYPES_TO_SNP_FILE, coverage1kbFile);
+    public static Tuple2<TextFile, TextFile> annotateCovWindows(CoverageTextFile coverage1kbFile) {
+        return (Tuple2<TextFile, TextFile>) GenericMethod.callGenericTool("annotateCovWindows", coverage1kbFile);
     }
 
     @ScriptCallingMethod
-    public static Tuple2<TextFile, TextFile> mergeAndFilterCovWindows(TextFile annotatedCoverageFile) {
-        return (Tuple2<TextFile, TextFile>) GenericMethod.callGenericTool(COConstants.TOOL_ADD_HAPLOTYPES_TO_SNP_FILE, annotatedCoverageFile);
+    public static TextFile mergeAndFilterCovWindows(TextFile annotatedCoverageFile) {
+        return (TextFile) GenericMethod.callGenericTool("mergeAndFilterCovWindows", annotatedCoverageFile);
     }
 
     @ScriptCallingMethod
-    public static Tuple3<TextFile, TextFile, TextFile> correctGC(TextFile mergedAndFilteredCovWinFile) {
-        return (Tuple3<TextFile, TextFile, TextFile>) GenericMethod.callGenericTool(COConstants.TOOL_CORRECT_GC_BIAS, mergedAndFilteredCovWinFile);
+    public static Tuple3<TextFile, TextFile, TextFile> correctGc(TextFile mergedAndFilteredCovWinFile) {
+        return (Tuple3<TextFile, TextFile, TextFile>) GenericMethod.callGenericTool("correctGc", mergedAndFilteredCovWinFile);
     }
+
+    private static Tuple3<TextFile, TextFile, TextFile>  aceSeqQc(CoverageTextFile windowedCoverageTextFile) {
+        Tuple2<TextFile, TextFile> annotationResult = ACEseqMethods.annotateCovWindows(windowedCoverageTextFile);
+        TextFile mergedAndFilteredCoverageWindowFiles = ACEseqMethods.mergeAndFilterCovWindows(annotationResult.value0);
+        Tuple3<TextFile, TextFile, TextFile> correctedWindowFile = ACEseqMethods.correctGc(mergedAndFilteredCoverageWindowFiles);
+        return new Tuple3(annotationResult, mergedAndFilteredCoverageWindowFiles, correctedWindowFile)
+    }
+
+
 
 }
 
