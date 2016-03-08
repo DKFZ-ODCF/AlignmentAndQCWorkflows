b1d6b2c0f88f83534d5e530f5a26f7d1984d4efd
Philip R. Kensche <kensche@dkfz-heidelberg.de>
2016-03-08 10:09:05 +0100

    Added SAMPLE dynamic parameter to ACEseqQC jobs.

--- src/de/dkfz/b080/co/files/AlignmentConstants.java
+++ src/de/dkfz/b080/co/files/AlignmentConstants.java
@@ -3,5 +3,16 @@ package de.dkfz.b080.co.files;
 /**
  * Created by kensche on 04.03.16.
  */
-public class AlignmentConstants {
+public final class AlignmentConstants {
+
+    public static final String CVALUE_RUN_COVERAGE_PLOTS_ONLY = "runCoveragePlotsOnly";
+    public static final String CVALUE_OVERRIDE_FASTQ_FILES = "overrideFastqFiles";
+    public static final String CVALUE_OVERRIDE_BAM_FILES = "overrideBamFiles";
+    public static final String CVALUE_OVERRIDE_SAMPLE_NAMES = "overrideSampleNames";
+    public static final String CVALUE_MAPPABILITY_FILE = "MAPPABILITY_FILE";
+    public static final String CVALUE_REPLICATION_TIME_FILE = "REPLICATION_TIME_FILE";
+    public static final String CVALUE_GC_CONTENT_FILE = "GC_CONTENT_FILE";
+    public static final String CVALUE_RUN_ACESEQ_QC = "runACEseqQc";
+
+    private AlignmentConstants() {}
 }
--- src/de/dkfz/b080/co/qcworkflow/ACEseqMethods.groovy
+++ src/de/dkfz/b080/co/qcworkflow/ACEseqMethods.groovy
@@ -3,7 +3,8 @@ package de.dkfz.b080.co.qcworkflow
 import de.dkfz.b080.co.files.*;
 import de.dkfz.roddy.execution.jobs.*;
 import de.dkfz.roddy.knowledge.files.Tuple2;
-import de.dkfz.roddy.knowledge.files.Tuple3;
+import de.dkfz.roddy.knowledge.files.Tuple3
+import de.dkfz.roddy.knowledge.files.Tuple4;
 import de.dkfz.roddy.knowledge.methods.GenericMethod
 
 
@@ -12,8 +13,8 @@ public final class ACEseqMethods {
 
 
     @ScriptCallingMethod
-    public static Tuple2<TextFile, TextFile> annotateCovWindows(CoverageTextFile coverage1kbFile) {
-        return (Tuple2<TextFile, TextFile>) GenericMethod.callGenericTool("annotateCovWindows", coverage1kbFile);
+    public static Tuple2<TextFile, TextFile> annotateCovWindows(CoverageTextFile windowCoverageTextFile, Sample sample) {
+        return (Tuple2<TextFile, TextFile>) GenericMethod.callGenericTool("annotateCovWindows", windowCoverageTextFile, "SAMPLE=" + sample.name);
     }
 
     @ScriptCallingMethod
@@ -22,18 +23,16 @@ public final class ACEseqMethods {
     }
 
     @ScriptCallingMethod
-    public static Tuple3<TextFile, TextFile, TextFile> correctGc(TextFile mergedAndFilteredCovWinFile) {
-        return (Tuple3<TextFile, TextFile, TextFile>) GenericMethod.callGenericTool("correctGc", mergedAndFilteredCovWinFile);
+    public static Tuple4<TextFile, TextFile, TextFile, TextFile> correctGc(TextFile mergedAndFilteredCovWinFile, Sample sample) {
+        return (Tuple4<TextFile, TextFile, TextFile, TextFile>) GenericMethod.callGenericTool("correctGc", mergedAndFilteredCovWinFile, "SAMPLE=" + sample.name);
     }
 
-    private static Tuple3<TextFile, TextFile, TextFile>  aceSeqQc(CoverageTextFile windowedCoverageTextFile) {
-        Tuple2<TextFile, TextFile> annotationResult = ACEseqMethods.annotateCovWindows(windowedCoverageTextFile);
-        TextFile mergedAndFilteredCoverageWindowFiles = ACEseqMethods.mergeAndFilterCovWindows(annotationResult.value0);
-        Tuple3<TextFile, TextFile, TextFile> correctedWindowFile = ACEseqMethods.correctGc(mergedAndFilteredCoverageWindowFiles);
-        return new Tuple3(annotationResult, mergedAndFilteredCoverageWindowFiles, correctedWindowFile)
+    private static Tuple3<TextFile, TextFile, TextFile> aceSeqQc(CoverageTextFile windowedCoverageTextFile, Sample sample) {
+        Tuple2<TextFile, TextFile> annotationResult = ACEseqMethods.annotateCovWindows(windowedCoverageTextFile, sample);
+        TextFile mergedAndFilteredCoverageWindowFile = ACEseqMethods.mergeAndFilterCovWindows(annotationResult.value0);
+        Tuple3<TextFile, TextFile, TextFile, TextFile> correctedWindowFile = ACEseqMethods.correctGc(mergedAndFilteredCoverageWindowFile, sample);
+        return new Tuple3(annotationResult, mergedAndFilteredCoverageWindowFile, correctedWindowFile)
     }
-
-
 
 }
 
