523df8bd01421c3c228d66e488f7b5023d235709
Philip R. Kensche <kensche@dkfz-heidelberg.de>
2016-03-15 13:08:02 +0100

    Whitespace only.

--- resources/analysisTools/qcPipeline/cnvMergeFilter.sh
+++ resources/analysisTools/qcPipeline/cnvMergeFilter.sh
@@ -7,7 +7,7 @@ set -x
 ${PYTHON_BINARY} "${TOOL_MERGE_FILTER_CNV}" \
             --inputfile    "$FILENAME_COV_WINDOWS_1KB_ANNO" \
             --output       "$FILENAME_COV_WINDOWS_WG" \
-	        --coverage     "$cnv_min_coverage" \
+            --coverage     "$cnv_min_coverage" \
             --mappability  "$mapping_quality" \
             --NoOfWindows  "$min_windows"
 
