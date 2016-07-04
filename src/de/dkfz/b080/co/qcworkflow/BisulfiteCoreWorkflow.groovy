cd3cd6340156624b03bd3bbf3e1ea2c0e8d064a7
Philip R. Kensche <kensche@dkfz-heidelberg.de>
2016-07-04 14:43:03 +0200

    Minor changes required to get old workflow running in test setting, including bugfixes that fix the only QC bug.
    Most changes have similarly been done with the current workflow and were backported.
    
    Conflicts:
    	resources/analysisTools/qcPipeline/bwaMemSortSlim.sh
    	resources/analysisTools/qcPipeline/mergeAndMarkOrRemoveDuplicatesSlim.sh
    	resources/configurationFiles/analysisQC.xml
    	src/de/dkfz/b080/co/qcworkflow/QCPipeline.groovy

--- src/de/dkfz/b080/co/qcworkflow/BisulfiteCoreWorkflow.groovy
+++ src/de/dkfz/b080/co/qcworkflow/BisulfiteCoreWorkflow.groovy
@@ -7,6 +7,7 @@ import de.dkfz.roddy.config.RecursiveOverridableMapContainerForConfigurationValu
 import de.dkfz.roddy.core.DataSet
 import de.dkfz.roddy.core.ExecutionContext
 import de.dkfz.roddy.core.ExecutionContextError
+import sun.reflect.generics.reflectiveObjects.NotImplementedException
 
 import static de.dkfz.b080.co.files.COConstants.FLAG_EXTRACT_SAMPLES_FROM_OUTPUT_FILES
 
@@ -31,14 +32,15 @@ public class BisulfiteCoreWorkflow extends QCPipeline {
 
         COProjectsRuntimeService runtimeService = (COProjectsRuntimeService) context.getProject().getRuntimeService();
 
-        List<Sample> samples = runtimeService.getSamplesForContext(context);
+        List<Sample> samples = runtimeService.getSamplesForRun(context);
 
         BamFileGroup mergedBamFiles = new BamFileGroup();
         Map<Sample.SampleType, CoverageTextFileGroup> coverageTextFilesBySample = [:]
 
         for (Sample sample in samples) {
 
-            List<String> availableLibrariesForSample = runtimeService.getLibrariesForSample(sample);
+            throw new NotImplementedException()
+            List<String> availableLibrariesForSample = null /* Not implemented: runtimeService.getLibrariesForRun(sample) */
             BamFileGroup mergedBamsPerLibrary = new BamFileGroup();
 
             // Create per library merged bams
@@ -108,7 +110,8 @@ public class BisulfiteCoreWorkflow extends QCPipeline {
         String sampleID = sample.getName() + "_" + library;
         Map<String, List<LaneFileGroup>> mapForDataSet = foundRawSequenceFileGroups.get(dataSet);
         if (!mapForDataSet.containsKey(sampleID)) {
-            List<LaneFileGroup> laneFileGroups = sample.getLanes(library);
+            throw new NotImplementedException()
+            List<LaneFileGroup> laneFileGroups = null /* Not Implemented: sample.getLanes(library) */
             mapForDataSet.put(sampleID, laneFileGroups);
         }
 
@@ -128,7 +131,7 @@ public class BisulfiteCoreWorkflow extends QCPipeline {
     @Override
     public boolean checkExecutability(ExecutionContext context) {
         COProjectsRuntimeService runtimeService = (COProjectsRuntimeService) context.getProject().getRuntimeService();
-        List<Sample> samples = runtimeService.getSamplesForContext(context);
+        List<Sample> samples = runtimeService.getSamplesForRun(context);
         if (samples.size() == 0)
             return false;
 
@@ -136,10 +139,12 @@ public class BisulfiteCoreWorkflow extends QCPipeline {
         int cnt = 0;
         for (Sample sample : samples) {
             List<LaneFileGroup> laneFileGroups = [];
+            throw new NotImplementedException()
+            /* Not implemented Sample.getLibraries()
             for (String lib : sample.getLibraries()) {
                 laneFileGroups += loadLaneFilesForSampleAndLibrary(context, sample, lib);
             }
-
+            */
             for (LaneFileGroup lfg : laneFileGroups) {
                 cnt += lfg.getFilesInGroup().size();
             }
