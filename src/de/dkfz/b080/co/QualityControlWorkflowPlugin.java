6cc630d8ff65c51183ca8a07cea77df05836c6de
Philip R. Kensche <kensche@dkfz-heidelberg.de>
2016-07-04 12:37:35 +0200

    Binaries.

--- src/de/dkfz/b080/co/QualityControlWorkflowPlugin.java
+++ src/de/dkfz/b080/co/QualityControlWorkflowPlugin.java
@@ -9,7 +9,7 @@ import de.dkfz.roddy.plugins.BasePlugin;
 public class QualityControlWorkflowPlugin extends BasePlugin {
 
     public static final String CURRENT_VERSION_STRING = "1.0.178";
-    public static final String CURRENT_VERSION_BUILD_DATE = "Mon Jul 04 08:57:28 CEST 2016";
+    public static final String CURRENT_VERSION_BUILD_DATE = "Mon Jul 04 11:27:34 CEST 2016";
 
     @Override
     public String getVersionInfo() {
