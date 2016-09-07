f95a5efdbb3086eef759d9e4276ad8cbd95bb0a1
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2016-09-07 17:06:14 +0200

    Make sure that higher priority columns are unique in metadata table, whenever rows are selected.

--- src/de/dkfz/b080/co/QualityControlWorkflowPlugin.java
+++ src/de/dkfz/b080/co/QualityControlWorkflowPlugin.java
@@ -8,8 +8,8 @@ import de.dkfz.roddy.plugins.BasePlugin;
  */
 public class QualityControlWorkflowPlugin extends BasePlugin {
 
-	public static final String CURRENT_VERSION_STRING = "1.1.51";
-	public static final String CURRENT_VERSION_BUILD_DATE = "Thu Aug 04 09:07:32 CEST 2016";
+	public static final String CURRENT_VERSION_STRING = "1.1.52";
+	public static final String CURRENT_VERSION_BUILD_DATE = "Tue Sep 06 10:17:34 CEST 2016";
 
     @Override
     public String getVersionInfo() {
