169429040be6ab8ae1a9d194e0ad5a4619fdfc7e
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2016-09-09 15:59:10 +0200

    Build with fingerprint support.

--- src/de/dkfz/b080/co/QualityControlWorkflowPlugin.java
+++ src/de/dkfz/b080/co/QualityControlWorkflowPlugin.java
@@ -8,8 +8,8 @@ import de.dkfz.roddy.plugins.BasePlugin;
  */
 public class QualityControlWorkflowPlugin extends BasePlugin {
 
-	public static final String CURRENT_VERSION_STRING = "1.1.59";
-	public static final String CURRENT_VERSION_BUILD_DATE = "Thu Sep 08 16:09:32 CEST 2016";
+	public static final String CURRENT_VERSION_STRING = "1.1.60";
+	public static final String CURRENT_VERSION_BUILD_DATE = "Fri Sep 09 15:58:58 CEST 2016";
 
     @Override
     public String getVersionInfo() {
