033121a6ae1a50fc6dbf3afa934bc3947b608167
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2017-08-11 18:01:49 +0200

    Moved some _BINARY variables from the XML into environment scripts. Scripts for the TBI PBS and the new LSF clusters are roughly done.

---
+++ resources/analysisTools/qcPipeline/environments/tbi-lsf-cluster.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+module load R/3.0.0
+module load bwa/"${BWA_VERSION:-0.7.8}"      # select version!
+module load java/1.8.0_131
+module load fastqc/1.11.3
+module load perl/5.20.2
+module load python/2.7.9
+module load samtools/0.1.19
+module load htslib/0.2.5
+module load bedtools/2.16.2
+
+# Also used for duplication marking.
+module load sambamba/0.5.9
+
+
+if markWithBiobambam; then
+    module load libmaus/0.0.130
+    module load biobambam/0.0.148
+elif markWithPicard; then
+    module load picard/1.125
+else
+    echo "Oops" > /dev/stderr
+fi
+
+if [[ "WGBS" ]]; then
+    module load moabs/1.3.0
+fi
+
+# The sambamba version used for sorting, viewing. Note that v0.5.9 is segfaulting on convey during view or sort.
+export SAMBAMBA_BINARY=sambamba
+
+# The sambamba version used only for making flagstats.
+export SAMBAMBA_FLAGSTATS_BINARY=sambamba
+# Warning: Currently bwaMemSortSlim uses sambamba flagstats, while mergeAndMarkOrRemoveSlim uses samtools flagstats.
+
+# The sambamba version used only for duplication marking and merging. Use the bash function here!
+export SAMBAMBA_MARKDUP_BINARY=sambamba
+
+export PICARD_BINARY=picard.sh
+export FASTQC_BINARY=fastqc
+export SAMTOOLS_BINARY=samtools
+
+# biobambam
+export BAMSORT_BINARY=bamsort
+export MARKDUPLICATES_BINARY=bammarkduplicates
+
+export JAVA_BINARY=java
+
+# htslib/0.2.5
+export BGZIP_BINARY=bgzip
+export TABIX_BINARY=tabix
+
+export PERL_BINARY=perl
+export PYTHON_BINARY=python
+export PYPY_BINARY=pypy-c
+export RSCRIPT_BINARY=Rscript
+export MBUFFER_BINARY=mbuffer
+
+#export FASTAFROMBED_BINARY=fastaFromBed-2.16.2
+#export BCFTOOLS_BINARY=bcftools-0.1.19
+#export VCFTOOLS_SORT_BINARY=vcf-sort-0.1.10
+
+# WES only
+export INTERSECTBED_BINARY=intersectBed
+export COVERAGEBED_BINARY=coverageBed
+
+if [[ WGBS ]]; then
+    export BWA_BINARY=bwa-0.7.8-bisulfite
+else
+    export BWA_BINARY=bwa-0.7.8
+    export BWA_ACCELERATED_BINARY=/opt/bb/bwa-0.7.8-r2.05/bin/bwa-bb
+fi
+
---
+++ resources/analysisTools/qcPipeline/environments/tbi-pbs-cluster.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# The sambamba version used for sorting, viewing. Note that v0.5.9 is segfaulting on convey during view or sort.
+export SAMBAMBA_BINARY=sambamba_v0.4.6
+
+# The sambamba version used only for making flagstats.
+export SAMBAMBA_FLAGSTATS_BINARY=sambamba_v0.4.6
+# Warning: Currently bwaMemSortSlim uses sambamba flagstats, while mergeAndMarkOrRemoveSlim uses samtools flagstats.
+
+# The sambamba version used only for duplication marking and merging.
+export SAMBAMBA_MARKDUP_BINARY=sambamba_v0.5.9
+
+export PICARD_BINARY=picard-1.125.sh
+export FASTQC_BINARY=fastqc-0.10.1
+export SAMTOOLS_BINARY=samtools-0.1.19
+
+# biobambam
+export BAMSORT_BINARY=bamsort-0.0.148
+export MARKDUPLICATES_BINARY=bammarkduplicates-0.0.148
+
+export JAVA_BINARY=java8
+
+# htslib/0.2.5
+export BGZIP_BINARY=bgzip-tabix-0.2.5
+export TABIX_BINARY=tabix-0.2.5
+
+export PERL_BINARY=perl-5.20.2
+export PYTHON_BINARY=python-2.7.9
+export PYPY_BINARY=pypy-c
+export RSCRIPT_BINARY=Rscript-3.0.0
+export MBUFFER_BINARY=mbuffer
+
+#export FASTAFROMBED_BINARY=fastaFromBed-2.16.2
+#export BCFTOOLS_BINARY=bcftools-0.1.19
+#export VCFTOOLS_SORT_BINARY=vcf-sort-0.1.10
+
+# WES only
+export INTERSECTBED_BINARY=intersectBed-2.16.2
+export COVERAGEBED_BINARY=coverageBed-2.16.2
+
+if [[ WGBS ]]; then
+    export BWA_BINARY=bwa-0.7.8-bisulfite
+else
+    export BWA_BINARY=bwa-0.7.8
+    export BWA_ACCELERATED_BINARY=/opt/bb/bwa-0.7.8-r2.05/bin/bwa-bb
+fi
+
+
+
