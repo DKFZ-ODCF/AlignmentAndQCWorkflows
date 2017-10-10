cf444e405635782237394301cfc6f5f5422e427d
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2017-10-10 14:32:58 +0200

    Fixed FASTQC tool.

--- resources/analysisTools/qcPipeline/checkFastQC.sh
+++ resources/analysisTools/qcPipeline/checkFastQC.sh
@@ -4,27 +4,30 @@
 #PBS -l nodes=1
 #PBS -m a
 
+set -uvex
+
 source "$TOOL_WORKFLOW_LIB"
 printInfo
 
 # Check for single lane processing.
 # For single lanes an empty fastqc file is created
-[[ -z "${useSingleEndProcessing+x}" && ! -f ${RAW_SEQ} ]] && touch ${FILENAME_FASTQC} && exit 0
+if [[ -v useSingleEndProcessing && "$useSingleEndProcessing" == true && ! -f ${RAW_SEQ} ]]; then
+    touch ${FILENAME_FASTQC}
+else
+    # by default, fastqc writes a zipped file with the infile name as prefix to the outdir
 
-# by default, fastqc writes a zipped file with the infile name as prefix to the outdir
+    DIRNAME_FASTQC=`dirname $FILENAME_FASTQC`
+    TMP_DIR=$DIRNAME_FASTQC/$RODDY_JOBID
+    mkdir "$TMP_DIR"
 
-DIRNAME_FASTQC=`dirname $FILENAME_FASTQC`
+    ${FASTQC_BINARY} ${RAW_SEQ} --noextract -o $TMP_DIR \
+        || throw 1 "Error during FASTQC"
 
-TMP_DIR=$DIRNAME_FASTQC/$RODDY_JOBID
-mkdir $TMP_DIR
+    mv $TMP_DIR/*.zip $FILENAME_FASTQC
 
-${FASTQC_BINARY} ${RAW_SEQ} --noextract -o $TMP_DIR \
-    || throw 1 "Error during FASTQC"
+    "${TOOL_FASTQC_CLASSIFY}" \
+        <(unzip -p "${FILENAME_FASTQC}" "*/fastqc_data.txt") \
+        > "${FILENAME_FASTQ_QC_STATUS}" || throw 10 "Error classifying the FASTQ quality"
 
-mv $TMP_DIR/* $FILENAME_FASTQC
-
-"${TOOL_FASTQC_CLASSIFY}" \
-    <(unzip -p "${FILENAME_FASTQC}" "*/fastqc_data.txt") \
-    > "${FILENAME_FASTQ_QC_STATUS}" || throw 10 "Error classifying the FASTQ quality"
-
-rm -r $TMP_DIR
+    rm -r "$TMP_DIR"
+fi
