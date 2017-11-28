6d9b967768924004e5bc8fb60d339f9bb5844af4
Philip R. Kensche <pkensche@kanthi.de>
2017-11-28 16:24:56 +0100

    More changes to get the workflow running on the new cluster.

--- resources/analysisTools/qcPipeline/checkFastQC.sh
+++ resources/analysisTools/qcPipeline/checkFastQC.sh
@@ -24,10 +24,5 @@ else
         || throw 1 "Error during FASTQC"
 
     mv $TMP_DIR/*.zip $FILENAME_FASTQC
-
-    "${TOOL_FASTQC_CLASSIFY}" \
-        <(unzip -p "${FILENAME_FASTQC}" "*/fastqc_data.txt") \
-        > "${FILENAME_FASTQ_QC_STATUS}" || throw 10 "Error classifying the FASTQ quality"
-
     rm -r "$TMP_DIR"
 fi
