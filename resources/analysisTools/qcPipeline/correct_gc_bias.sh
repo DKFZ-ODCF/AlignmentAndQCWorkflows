df8255825b3470741b8ebc51e70dbc26a2aa562b
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2017-12-01 17:21:22 +0100

    Fixed wrong output file name causing rerun of correcGc job.

--- resources/analysisTools/qcPipeline/correct_gc_bias.sh
+++ resources/analysisTools/qcPipeline/correct_gc_bias.sh
@@ -5,7 +5,7 @@ source "$TOOL_WORKFLOW_LIB"
 set -x
 
 tmp_corrected_windowfile="${FILENAME_GC_CORRECTED_WINDOWS}.tmp"
-corrected_table_slim="${FILENAME_GC_CORRECTED_QUALITY}.slim.txt"
+tmp_corrected_table_slim="${FILENAME_GC_CORRECTED_QUALITY}.tmp"
 
 ${RSCRIPT_BINARY} "$TOOL_CORRECT_GC_BIAS_R" \
 	--windowFile	"$FILENAME_COV_WINDOWS_WG" \
@@ -15,7 +15,7 @@ ${RSCRIPT_BINARY} "$TOOL_CORRECT_GC_BIAS_R" \
 	--sample	$(sampleType "$SAMPLE") \
 	--outfile	"$tmp_corrected_windowfile" \
 	--corPlot	"$FILENAME_GC_CORRECT_PLOT" \
-	--qcTab		"$corrected_table_slim" \
+	--qcTab		"$tmp_corrected_table_slim" \
 	--gcFile	"$GC_CONTENT_FILE_ALN" \
 	--outDir	"$aceseqOutputDirectory" \
 	--lowess_f	"$LOWESS_F_ALN" \
@@ -34,7 +34,7 @@ mv "$tmp_corrected_windowfile" "$FILENAME_GC_CORRECTED_WINDOWS"
 tmp_qc_value_file="$FILENAME_QC_GC_CORRECTION_JSON.tmp"
 
 ${PYTHON_BINARY} "$TOOL_CONVERT_TO_JSON" \
-	--file 	"$corrected_table_slim" \
+	--file 	"$tmp_corrected_table_slim" \
 	--id 	"$pid" \
 	--out 	"$tmp_qc_value_file" \
 	--key	"$GC_bias_json_key_ALN"
@@ -46,4 +46,4 @@ then
 fi	
 
 mv "$tmp_qc_value_file" "$FILENAME_QC_GC_CORRECTION_JSON"
-
+mv "$tmp_corrected_table_slim" "$FILENAME_GC_CORRECTED_QUALITY"
