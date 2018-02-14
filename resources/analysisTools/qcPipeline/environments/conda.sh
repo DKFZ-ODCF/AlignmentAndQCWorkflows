62d0ae0a9635f8c072a14ea415896a8a5c3df99c
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2018-02-14 17:11:15 +0100

    Update conda.sh and conda.yml.

--- resources/analysisTools/qcPipeline/environments/conda.sh
+++ resources/analysisTools/qcPipeline/environments/conda.sh
@@ -4,25 +4,6 @@
 # Distributed under the MIT License (license terms are at https://www.github.com/eilslabs/ACEseqWorkflow/LICENSE.txt).
 # Load a Conda environment.
 
-createCleanCondaEnvironment () {
-    unset LD_LIBRARY_PATH
-    unset MODULE_PATH
-    unset PKG_CONFIG_PATH
-    unset PYTHONHOME
-    unset PYTHONPATH
-    unset PYTHONSTARTUP
-    unset PYTHON_LIB
-    unset PBS_SHARED_BIN
-    unset PERL5LIB
-    unset PERL_LOCAL_LIB_ROOT
-    unset PERL_MB_OPT
-    unset PERL_MM_OPT
-    export PATH="$HOME/miniconda3/bin:/opt/torque/bin:/usr/lib64/mpi/gcc/openmpi/bin:/opt/maui/bin:/usr/local/bin:/usr/bin:/bin:/usr/bin/X11:/usr/X11R6/bin:/usr/lib/mit/bin"
-}
-
-createCleanCondaEnvironment
-
-
 source activate "${condaEnvironmentName:?No Conda environment name defined. Please set 'condaEnvironmentName'.}" \
     || (echo "Could not load Conda environment '$condaEnvironmentName'" && exit 100)
 
