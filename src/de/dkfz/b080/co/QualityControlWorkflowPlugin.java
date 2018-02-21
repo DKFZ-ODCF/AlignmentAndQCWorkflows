4b856d4cf2f15b93b5def428860e9dba03c41369
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2018-02-21 16:10:20 +0100

    Updated to Roddy 3.0. Recompile.

--- src/de/dkfz/b080/co/QualityControlWorkflowPlugin.java
+++ src/de/dkfz/b080/co/QualityControlWorkflowPlugin.java
@@ -8,8 +8,8 @@ import de.dkfz.roddy.plugins.BasePlugin;
  */
 public class QualityControlWorkflowPlugin extends BasePlugin {
 
-    public static final String CURRENT_VERSION_STRING = "1.2.184";
-    public static final String CURRENT_VERSION_BUILD_DATE = "Fri Feb 02 10:26:01 CET 2018";
+    public static final String CURRENT_VERSION_STRING = "1.2.182";
+    public static final String CURRENT_VERSION_BUILD_DATE = "Wed Feb 21 16:10:06 CET 2018";
 
     @Override
     public String getVersionInfo() {
