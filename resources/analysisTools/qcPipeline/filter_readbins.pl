2457dde8271d1e8a0ff9d5447d5341ad930e9aa1
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2019-10-02 18:34:03 +0200

    Editing fix.

--- resources/analysisTools/qcPipeline/filter_readbins.pl
+++ resources/analysisTools/qcPipeline/filter_readbins.pl
@@ -61,8 +61,9 @@ my $kept = 0;
 my $timestamp = localtime();
 print STDERR "($timestamp) filter_readbins.pl is waiting for input...\n";
 
+my $line = "";
 while (! eof($binsFh)) {
-    my $line = readline($binsFh)
+    $line = readline($binsFh)
             || croak "Couldn't read bins file '$binsfile': $!";
     $all++;
     my @fields = split("\t", $line);
@@ -77,7 +78,7 @@ while (! eof($binsFh)) {
 $binsFh->close;
 
 $timestamp = localtime();
-print STDERR "($timestamp) filter_readbins.pl finished reading bins file";
+print STDERR "($timestamp) filter_readbins.pl finished reading bins file\n";
 print STDERR "Content of last line was ---'$line'---\n";
 print STDERR "Chromosomes in read-bins file: " . join(" ", sort keys %readBinsChr) . "\n";
 print STDERR "From $all lines, kept $kept with selected chromosomes\n";
